452fabdce06bcab19de7114c9f019d9144bbd263
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-25 15:46:56 +0200

    dev: added layout components

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 import HeaderLayout from './components/layout/HeaderLayout.vue'
+import GalleryLayout from './components/layout/GalleryLayout.vue'
+import SidebarLayout from './components/layout/SidebarLayout.vue'
+import DashboardLayout from './components/layout/DashboardLayout.vue'
 </script>
 
 <template>
   <HeaderLayout />
+  <GalleryLayout />
+  <div class="content">
+    <SidebarLayout />
+    <DashboardLayout />
+  </div>
 </template>
 
 <style scoped></style>
