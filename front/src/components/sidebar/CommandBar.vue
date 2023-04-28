1fec43c9bd6140f9d8e195b05fb30cdb275d17df
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-28 14:56:32 +0200

    dev: added select

--- front/src/components/sidebar/CommandBar.vue
+++ front/src/components/sidebar/CommandBar.vue
@@ -14,6 +14,15 @@ const configStore = useConfigStore()
       <span>Limit: {{ configStore.limit }}</span>
       <input type="range" min="0.01" max="5" step="0.01" v-model="configStore.limit" />
     </label>
+    <label>
+      <span>Color scheme: </span>
+      <select>
+        <option value="x">Tu choisis x</option>
+        <option value="y">Tu choisis y</option>
+        <option value="z">Tu choisis z</option>
+        <option value="t">Tu choisis t</option>
+      </select>
+    </label>
   </div>
 </template>
 
@@ -28,6 +37,13 @@ div.command {
   label {
     display: flex;
     flex-flow: column;
+
+    select {
+      border: 0.1em solid black;
+      border-radius: 0.3em;
+      font-size: 1.1em;
+      padding: 0.5em 1em;
+    }
   }
 }
 </style>
